88b54bb294c267310861f5b7aa917eca7d01ec63
Blacklisteddeveloper <blacklisted.web02@gmail.com>
2018-06-18 18:47:59 +0530

    updates 18-06

---
+++ templates/template-pricing.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Template Name: Pricing Page
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package costcertified
+ */
+get_header(); ?>
+
+<section class="pricing_page">
+	<section class="pricing">
+		<div class="heading">
+			<h3>Get CostCertified for your next project.</h3>
+			<h2>Pricing</h2>
+		</div>
+		<ul>
+			<li class="small">
+				<div class="icon"><img src="<?php bloginfo('template_url'); ?>/assets/svg/pricing_1.svg" /></div>
+				<span class="heading">Customer</span>
+				<ul class="list">
+					<li>Contractors using cost certified will know their costs.</li>
+					<li>You will stick through the entire length of the job.</li>
+					<li>You will not need to ask for extra money throughout or at the end of the job.</li>
+				</ul>
+				<div class="price">
+					
+				</div>
+				<a href="#">INVITE YOUR CONTRACTOR</a>
+			</li>
+			<li class="big">
+				<div class="icon"><img src="<?php bloginfo('template_url'); ?>/assets/svg/pricing_2.svg" /></div>
+				<span class="heading">Contractor</span>
+				<ul class="list">
+					<li>Contractors using cost certified will know their costs.</li>
+					<li>You will stick through the entire length of the job.</li>
+					<li>You will not need to ask for extra money throughout or at the end of the job.</li>
+				</ul>
+				<div class="price">
+					<span>15</span>
+					<p>Per user per month,billed monthly<br/>or $12.5 per user if billed annually</p>
+				</div>
+				<a href="#">JOIN US</a>
+			</li>
+			<li class="small">
+				<div class="icon"><img src="<?php bloginfo('template_url'); ?>/assets/svg/pricing_3.svg" /></div>
+				<span class="heading">Contractor</span>
+				<ul class="list">
+					<li>Contractors using cost certified will know their costs.</li>
+					<li>You will stick through the entire length of the job.</li>
+					<li>You will not need to ask for extra money throughout or at the end of the job.</li>
+				</ul>
+				<div class="price">
+					<span>15</span>
+					<p>Per user per month,billed monthly<br/>or $12.5 per user if billed annually</p>
+				</div>
+				<a href="#">JOIN US</a>
+			</li>
+		</ul>
+	</section>
+</section>
+
+<?php get_footer(); 
